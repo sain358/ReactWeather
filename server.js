d3af5fd31ce06609398e5ba399417a4e28e342ae
sain358 <aleksandrs.podvinskis@gmail.com>
2019-12-01 12:01:26 +0200

    Prep for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.ev.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log('Express server is up on port 3000');
 });
